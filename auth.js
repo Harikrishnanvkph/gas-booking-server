0f6d92761b3b5e57ef05474b9a10642f0e39bb54
HarikrishnanVK <54284966+Harikrishnanvkph@users.noreply.github.com>
2024-06-10 12:20:14 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 //custom middleware to validate token
 const auth = (req,res,next)=>{
     const token = req.cookies.token;
+    console.log(req.cookies);
     if(!token){
         return res.send("404/NoValidation")
     }
